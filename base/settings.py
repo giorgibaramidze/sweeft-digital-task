952e467ad93feb2a634afc29b12cf96eede2dcff
Giorgi Baramidze <giorgi.baramidze0128@gmail.com>
2023-09-29 03:43:27 +0400

    customizing authentication

--- base/settings.py
+++ base/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -119,5 +121,5 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL  = 'accounts.Account'
